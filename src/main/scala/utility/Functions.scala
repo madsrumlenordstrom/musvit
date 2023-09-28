b1aa51eb090bd82b4356d44cca6c605d96eae5e5
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2023-09-28 23:35:55 +0200

    byte

--- src/main/scala/utility/Functions.scala
+++ src/main/scala/utility/Functions.scala
@@ -3,9 +3,10 @@ package utility
 import chisel3._
 import java.nio.file.Files
 import java.io.File
+import Constants.BYTE_W
 
 object Functions {
-  def fileToByteSeq(file: File) = Files.readAllBytes(file.toPath()).map(_ & 0xff).map(_.asUInt(8.W)).toSeq
+  def fileToByteSeq(file: File) = Files.readAllBytes(file.toPath()).map(_ & 0xff).map(_.asUInt(BYTE_W.W)).toSeq
 
   def bitWidthToUIntMax(width: Int) = ((1L << width.toLong) - 1L)
 }
