9abe881caa9f9afc260469d159dc4cf06a627d57
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2023-10-24 22:07:32 +0200

    RAM working

--- src/main/scala/memory/RAM.scala
+++ src/main/scala/memory/RAM.scala
@@ -1,11 +1,16 @@
 package memory
 
 import chisel3._
-import chisel3.util.log2Up
+import chisel3.util._
+import chisel3.util.experimental.decode._
 
 import musvit.MusvitConfig
+import musvit.common.OpCodes
 import utility.Constants._
 import utility.BitsToByteVec
+import utility.BarrelShifter
+import utility.SignExtend
+import utility.ZeroExtend
 
 class RAMIO(size: Int, width: Int) extends Bundle {
   val readAddr      = Input(UInt(log2Up(size).W))
@@ -39,29 +44,65 @@ object RAM {
   }
 }
 
-class MusvitRAMIO(config: MusvitConfig) extends Bundle {
+class MusvitRAMIO(config: MusvitConfig) extends Bundle with OpCodes {
   val addr          = Input(UInt(log2Up(config.ramSize).W))
   val en            = Input(Bool())
   val isWrite       = Input(Bool())
   val isSigned      = Input(Bool())
+  val dataWidth     = Input(UInt(Mem.X.getWidth.W))
+  val writeData     = Input(UInt(WORD_WIDTH.W))
 
-  val writeData     = Input(UInt(DATA_WIDTH.W))
-  val readData      = Output(UInt(DATA_WIDTH.W))  
-  
+  val readData      = Output(UInt(WORD_WIDTH.W))  
 }
 
-class MusvitRAM(config: MusvitConfig) extends Module {
+class MusvitRAM(config: MusvitConfig) extends Module with OpCodes {
   val io = IO(new MusvitRAMIO(config))
 
   // Addresses
-  val byteOffset = io.addr(log2Up(BYTES_PER_DATA) - 1, 0)
-
-  val mem = SyncReadMem(config.ramSize / BYTES_PER_DATA, Vec(BYTES_PER_DATA, UInt(BYTE_WIDTH.W)))
-
+  val byteOffset = io.addr(log2Up(BYTES_PER_WORD) - 1, 0)
   val addr = (io.addr - config.ramAddr.U)(io.addr.getWidth - 1, byteOffset.getWidth) // TODO: maybe find another solution to this
 
-  val dataVec = BitsToByteVec(io.writeData)
+  // Error checking
+  assert((byteOffset === 0.U || io.dataWidth =/= Mem.W) || !io.en, "Word RAM operation not correctly aligned")
+  assert((byteOffset(0) === 0.U || io.dataWidth =/= Mem.H) || !io.en, "Halfword RAM operation not correctly aligned")
 
-  val readData = mem.readWrite(addr, dataVec, io.en, io.isWrite)
+  val mem = SyncReadMem(config.ramSize / BYTES_PER_WORD, Vec(BYTES_PER_WORD, UInt(BYTE_WIDTH.W)))
+  
+  val writeData = BarrelShifter.rightShift(BitsToByteVec(io.writeData), byteOffset)
 
+  val maskTable = TruthTable(Map(
+    (Mem.B ## BitPat("b00")) -> BitPat("b0001"),
+    (Mem.B ## BitPat("b01")) -> BitPat("b0010"),
+    (Mem.B ## BitPat("b10")) -> BitPat("b0100"),
+    (Mem.B ## BitPat("b11")) -> BitPat("b1000"),
+    (Mem.H ## BitPat("b00")) -> BitPat("b0011"),
+    (Mem.H ## BitPat("b10")) -> BitPat("b1100"),
+    (Mem.W ## BitPat("b00")) -> BitPat("b1111"),
+  ),
+    BitPat("b????")
+  )
+
+  val maskVec = VecInit(decoder(EspressoMinimizer, io.dataWidth ## byteOffset, maskTable).asBools)
+  
+  when (io.en && io.isWrite) {
+    mem.write(addr, writeData, maskVec.toSeq)
+  }
+  
+  // Reading
+  val readData = mem.read(addr, io.en && !io.isWrite)
+  val byteOffsetReg = Reg(chiselTypeOf(byteOffset))
+  byteOffsetReg := byteOffset
+  val shifted = BarrelShifter.leftShift(readData, byteOffsetReg).asUInt
+  val isSignedReg = RegNext(io.isSigned)
+  val dataWidthReg = RegNext(io.dataWidth)
+  when (RegNext(io.en && !io.isWrite)) {
+    io.readData := MuxCase(shifted, Seq(
+      (isSignedReg && dataWidthReg === Mem.B) -> SignExtend(shifted, BYTE_WIDTH - 1, WORD_WIDTH),
+      (isSignedReg && dataWidthReg === Mem.H) -> SignExtend(shifted, HALF_WIDTH - 1, WORD_WIDTH),
+      (!isSignedReg && dataWidthReg === Mem.B) -> shifted(BYTE_WIDTH - 1, 0),
+      (!isSignedReg && dataWidthReg === Mem.H) -> shifted(HALF_WIDTH - 1, 0),
+    ))
+  }.otherwise {
+    io.readData := 0.U // Maybe remove?
+  }
 }
--- src/test/scala/memory/RAMTester.scala
+++ src/test/scala/memory/RAMTester.scala
@@ -3,9 +3,12 @@ package memory
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
+
 import utility.Constants._
 import utility.Functions._
 import musvit.MusvitConfig
+import musvit.common.OpCodes
+import chisel3.util.is
 
 class RAMTester extends AnyFlatSpec with ChiselScalatestTester {
 
@@ -65,61 +68,72 @@ class RAMTester extends AnyFlatSpec with ChiselScalatestTester {
   }
 }
 
-class MusvitRAMTester extends AnyFlatSpec with ChiselScalatestTester {
+class MusvitRAMTester extends AnyFlatSpec with ChiselScalatestTester with OpCodes {
 
   // Test configuration
   val testFile = "random"
-  val width = BYTE_WIDTH
-  val words = fileToUInts(testFile, width)
-  val config = MusvitConfig()
+  val bytes = fileToUInts(testFile, BYTE_WIDTH)
+  val halfs = fileToUInts(testFile, HALF_WIDTH)
+  val words = fileToUInts(testFile, WORD_WIDTH)
+  val config = MusvitConfig(ramAddr = 0x00000000, ramSize = 0x00002000)
 
-  "SyncRAM" should "pass" in {
+  "MusvitRAM" should "pass" in {
     test(new MusvitRAM(config)).withAnnotations(Seq(WriteVcdAnnotation, VerilatorBackendAnnotation)) { dut =>
 
-      def write(addr: UInt, data: UInt, steps: Int = 1): Unit = {
-        dut.io.writeEn.poke(true.B)
-        dut.io.writeAddr.poke(addr)
+      def write(addr: UInt, data: UInt, dataWidth: UInt, steps: Int = 1): Unit = {
+        dut.io.en.poke(true.B)
+        dut.io.isWrite.poke(true.B)
+        dut.io.dataWidth.poke(dataWidth)
+        dut.io.addr.poke(addr)
         dut.io.writeData.poke(data)
         dut.clock.step(steps)
       }
 
-      def read(addr: UInt, expected: UInt, steps: Int = 1): Unit = {
-        dut.io.readEn.poke(true.B)
-        dut.io.readAddr.poke(addr)
+      def read(addr: UInt, expected: UInt, dataWidth: UInt, isSigned: Bool, steps: Int = 1): Unit = {
+        dut.io.en.poke(true.B)
+        dut.io.isWrite.poke(false.B)
+        dut.io.isSigned.poke(isSigned)
+        dut.io.addr.poke(addr)
+        dut.io.dataWidth.poke(dataWidth)
         dut.clock.step(steps)
         dut.io.readData.expect(expected)
       }
 
+      def writeRAM(data: Seq[UInt], dataWidth: UInt, width: Int): Unit = {
+        for (i <- 0 until data.length) {
+          write((i * (width / BYTE_WIDTH)).asUInt(dut.io.addr.getWidth.W), data(i), dataWidth)
+        }      
+      }
+
+      def readRAM(data: Seq[UInt], dataWidth: UInt, width: Int): Unit = {
+        for (i <- 0 until data.length) {
+          read((i * (width / BYTE_WIDTH)).asUInt(dut.io.addr.getWidth.W), data(i), dataWidth, false.B)
+        }
+      }
+
+      def checkRAM(data: Seq[UInt], dataWidth: UInt, width: Int): Unit = {
+        writeRAM(data, dataWidth, width)
+        readRAM(data, dataWidth, width)
+      }
+
       dut.clock.setTimeout(0)
 
-      // Write to RAM
-      println("Writing to RAM")
-      for (i <- 0 until words.length) {
-        write(i.asUInt(dut.io.writeAddr.getWidth.W), words(i))
-      }
+      println("Checking bytes")
+      checkRAM(bytes, "b01".U, BYTE_WIDTH)
+      println("Checking halfs")
+      checkRAM(halfs, "b10".U, HALF_WIDTH)
+      println("Checking words")
+      checkRAM(words, "b00".U, WORD_WIDTH)
 
-      // Read RAM
-      println("Reading RAM")
-      for (i <- 0 until words.length) {
-        read(i.asUInt(dut.io.readAddr.getWidth.W), words(i))
-      }
-
-      // Read and write bypass checking
-      println("Checking bypass")
-      for (i <- 0 until words.length) {
-        val addr = i.asUInt(dut.io.readAddr.getWidth.W)
-        val word = words(words.length - 1 - i)
-        write(addr, word, 0)
-        read(addr, word, 1)
-      }
-
-      // Check data is still there
-      println("Reading RAM")
-      for (i <- 0 until words.length) {
-        val addr = i.asUInt(dut.io.readAddr.getWidth.W)
-        val word = words(words.length - 1 - i)
-        read(addr, word)
-      }
+      println("Checking sign extension")
+      write(0.U, "x80".U, "b01".U)
+      read(0.U,  "xFFFFFF80".U, "b01".U, true.B)
+      write(0.U, "x80".U, "b01".U)
+      read(0.U,  "x00000080".U, "b01".U, false.B)
+      write(0.U, "x8320".U, "b10".U)
+      read(0.U,  "xFFFF8320".U, "b10".U, true.B)
+      write(0.U, "x8320".U, "b10".U)
+      read(0.U,  "x00008320".U, "b10".U, false.B)
     }
   }
 }
