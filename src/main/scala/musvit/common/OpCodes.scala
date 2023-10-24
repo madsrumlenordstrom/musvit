e4c0ae2d34985f7596f6b33e7c0e1fef677170b0
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2023-10-24 22:08:11 +0200

    opcodes

--- src/main/scala/musvit/common/OpCodes.scala
+++ src/main/scala/musvit/common/OpCodes.scala
@@ -5,7 +5,15 @@ import chisel3.util._
 
 trait OpCodes {
   // Generic yes, no and don't care
-  def Y:      BitPat = BitPat("b1")
-  def N:      BitPat = BitPat("b0")
-  def X:      BitPat = BitPat("b?")
+  val Y:      BitPat = BitPat("b1")
+  val N:      BitPat = BitPat("b0")
+  val X:      BitPat = BitPat("b?")
+
+  // Memory opcodes
+  object Mem {
+    val B:  BitPat = BitPat("b01")
+    val H:  BitPat = BitPat("b10")
+    val W:  BitPat = BitPat("b00")
+    val X:  BitPat = BitPat("b??")
+  }
 }
