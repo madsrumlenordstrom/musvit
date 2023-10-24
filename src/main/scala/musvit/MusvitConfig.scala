b993391e00773afbd7caed42a4f18683983f4802
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2023-10-24 22:08:41 +0200

    default config

--- src/main/scala/musvit/MusvitConfig.scala
+++ src/main/scala/musvit/MusvitConfig.scala
@@ -7,4 +7,16 @@ case class MusvitConfig(
     romAddr: Long = 0x00000000L,    // Start address of ROM
     resetPC: Long = 0x00000000L,    // Initial PC value
     clockFrequency: Int = -1,       // Clock frequency of Musvit
+    ramSize: Int = 0x00001000,      // RAM size in bytes
+    ramAddr: Long = 0x00001000L,    // Start address of RAM
 )
+
+// TODO use BigInt instead of Int and Long for memory related stuff
+
+object MusvitConfigs {
+    val basys3 = MusvitConfig(
+        fetchWidth = 2,
+        romFile = "sw/build/blinky.bin",
+        clockFrequency = 100000000,
+    )
+}
