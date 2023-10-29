8a341d18932e99e600fa4326897c489440009f0d
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2023-10-29 13:06:26 +0100

    Small optimization for MultiOutputArbiter

--- src/main/scala/utility/MultiOutputArbiter.scala
+++ src/main/scala/utility/MultiOutputArbiter.scala
@@ -11,15 +11,16 @@ class MultiOutputArbiterIO[T <: Data](private val gen: T, val inputs: Int, val o
 class MultiOutputArbiter[T <: Data](private val gen: T, val inputs: Int, val outputs: Int) extends Module {
   val io = IO(new MultiOutputArbiterIO(gen, inputs, outputs))
   
-  val arbs = Seq.tabulate(outputs)( (i) => Module(new Arbiter(gen, inputs))) // - i
+  val arbs = Seq.tabulate(outputs)( (i) => Module(new Arbiter(gen, inputs - i)))
   arbs(0).io.in <> io.in
   io.out(0) <> arbs(0).io.out
   
   for (i <- 1 until outputs) {
-    arbs(i).io.in <> io.in
-    val chosenOH = scanRightOr(UIntToOH(arbs(i - 1).io.chosen)).unary_~.asBools
-    for (j <- 0 until io.in.length) {
-      arbs(i).io.in(j).valid := chosenOH(j) && io.in(j).valid
+    arbs(i).io.in <> VecInit(io.in.drop(i))
+    val chosenOH = scanRightOr(UIntToOH(arbs(i - 1).io.chosen)).unary_~.asBools.drop(1)
+
+    for (j <- 0 until arbs(i).io.in.length) {
+      arbs(i).io.in(j).valid := chosenOH(j) && io.in.drop(i)(j).valid
     }
     io.out(i) <> arbs(i).io.out
   }
