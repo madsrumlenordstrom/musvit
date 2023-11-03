72f76713e1059a9ed45454a29520f85f4dca05a5
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2023-11-03 23:04:31 +0100

    Reordered ALU to match opcode order

--- src/main/scala/musvit/execute/ALU.scala
+++ src/main/scala/musvit/execute/ALU.scala
@@ -5,6 +5,7 @@ import chisel3.util._
 
 import musvit.execute.FunctionalUnit
 import musvit.MusvitConfig
+import utility.Constants._
 
 class ALU (config: MusvitConfig) extends FunctionalUnit(config) {
   val shamt = data2(4, 0)
@@ -13,17 +14,17 @@ class ALU (config: MusvitConfig) extends FunctionalUnit(config) {
   val ltu = data1 < data2
   val eq = data1 === data2
 
-  io.result.bits := MuxCase(DontCare, Seq(
-    (op === ALU.ADD)  -> ((data1.asSInt + data2.asSInt).asUInt),
-    (op === ALU.SUB)  -> ((data1.asSInt - data2.asSInt).asUInt),
-    (op === ALU.AND)  -> (data1 & data2),
-    (op === ALU.OR)   -> (data1 | data2),
-    (op === ALU.XOR)  -> (data1 ^ data2),
-    (op === ALU.SLT)  -> (lt),
+  io.result.bits := MuxCase(0.U(WORD_WIDTH.W), Seq(
+    (op === ALU.ADD)  -> (data1 + data2),
+    (op === ALU.SUB)  -> (data1 - data2),
     (op === ALU.SLL)  -> (data1 << shamt),
+    (op === ALU.SLT)  -> (lt),
     (op === ALU.SLTU) -> (ltu),
+    (op === ALU.XOR)  -> (data1 ^ data2),
     (op === ALU.SRL)  -> (data1 >> shamt),
     (op === ALU.SRA)  -> ((data1.asSInt >> shamt).asUInt),
+    (op === ALU.OR)   -> (data1 | data2),
+    (op === ALU.AND)  -> (data1 & data2),
   ))
 
   io.result.valid := valid
